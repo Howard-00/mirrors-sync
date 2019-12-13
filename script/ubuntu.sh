1799140db20b59ff1471ecf8c8fcde47a97e3edc
Eric C <i@nonabyte.me>
2019-12-13 13:43:20 +0800

    mod_mirror:ubuntu:'Switch upstream to xTom_HK'

--- script/ubuntu.sh
+++ script/ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export sync_path="/mirrors"
-export sync_url="rsync://mirrors.tuna.tsinghua.edu.cn"
+export sync_url="rsync://mirrors.xtom.com.hk"
 export sync_name="ubuntu"
 export sync_command="rsync -rltz6 --progress --delete --log-file=$sync_path/log/$sync_name.log $sync_url/$sync_name $sync_path/data/$sync_name"
 sh "$sync_path/sync/script/rsync_common.sh"
