df66b17a8eb5e1d63529193c900aa7fe1ad1ba56
Eric C <i@nonabyte.me>
2020-01-13 18:06:23 +0800

    add_mirror:ubuntu-releases

---
+++ script/ubuntu-release.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export sync_path="/mirrors"
+export sync_url="rsync://mirrors.xtom.com.hk"
+export sync_name="ubuntu-releases"
+export sync_command="rsync -rltz6 --progress --delete-after --delay-updates --safe-links --contimeout=60  --log-file=$sync_path/log/$sync_name.log $sync_url/$sync_name $sync_path/data/$sync_name"
+sh "$sync_path/sync/script/rsync_common.sh"
