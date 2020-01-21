2ba46136f7084da25fe7bb62c841bc52b2f16694
Eric C <i@nonabyte.me>
2020-01-21 22:44:24 +0800

    mod_mirror:openwrt:'Switch to offical upstream'

--- script/openwrt.sh
+++ script/openwrt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export sync_path="/mirrors"
-export sync_url="rsync://rsync.mirrors.ustc.edu.cn/repo"
+export sync_url="rsync://proxy.uestclug.org"
 export sync_name="openwrt"
-export sync_upstream_name="lede"
-export sync_command="rsync -rltz6 --progress --delete-after --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r --delay-updates --safe-links --contimeout=60 --log-file=$sync_path/log/$sync_name.log $sync_url/$sync_upstream_name $sync_path/data/$sync_name"
+export proxy_port="9984"
+export sync_command="rsync -rltz6 --port=$proxy_port --progress --delete-after --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r --delay-updates --safe-links --contimeout=60 --log-file=$sync_path/log/$sync_name.log $sync_url/$sync_name $sync_path/data/$sync_name"
 bash "$sync_path/sync/script/rsync_common.sh"
