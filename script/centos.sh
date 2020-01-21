78e11802e1a3820bfb8e9293af3d06b3c3565d0f
Eric C <i@nonabyte.me>
2020-01-21 21:17:42 +0800

    mod_mirror:ALL:'Permission fixes and minor improvements'

--- script/centos.sh
+++ script/centos.sh
@@ -3,5 +3,5 @@
 export sync_path="/mirrors"
 export sync_url="rsync://mirrors.xtom.com.hk"
 export sync_name="centos"
-export sync_command="rsync -rltz6 --progress --delete --log-file=$sync_path/log/$sync_name.log $sync_url/$sync_name $sync_path/data/$sync_name"
+export sync_command="rsync -rltz6 --progress --delete-after --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r --delay-updates --safe-links --contimeout=60 --log-file=$sync_path/log/$sync_name.log $sync_url/$sync_name $sync_path/data/$sync_name"
 bash "$sync_path/sync/script/rsync_common.sh"
--- script/opensuse.sh
+++ script/opensuse.sh
@@ -3,5 +3,5 @@
 export sync_path="/mirrors"
 export sync_url="rsync://mirrors.tuna.tsinghua.edu.cn"
 export sync_name="opensuse"
-export sync_command="rsync -rltz6 --progress --delete --log-file=$sync_path/log/$sync_name.log $sync_url/$sync_name $sync_path/data/$sync_name"
+export sync_command="rsync -rltz6 --progress --delete-after --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r --delay-updates --safe-links --contimeout=60 --log-file=$sync_path/log/$sync_name.log $sync_url/$sync_name $sync_path/data/$sync_name"
 bash "$sync_path/sync/script/rsync_common.sh"
