6f15cf91991cd6a1455a02ca6d3e6b4d500cd5aa
h0cheung <36502569+h0cheung@users.noreply.github.com>
2020-06-11 17:25:25 +0800

    add elpa (tuna upstream)

---
+++ script/elpa.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+export sync_path="/mirrors"
+export sync_url="rsync://mirrors.tuna.tsinghua.edu.cn"
+export sync_name="elpa"
+export sync_command="rsync -rltz6 --progress --delete-after --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r --delay-updates --safe-links --contimeout=60 --log-file=$sync_path/log/$sync_name.log $sync_url/$sync_name $sync_path/data/$sync_name"
+bash "$sync_path/sync/script/rsync_common.sh"
